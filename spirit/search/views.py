8fb6a9c7b980c42fe16306fe1ae764aa92d99e6a
george <george.kandamkoalthy@gmail.com>
2021-12-13 13:22:20 +1100

    Final implementation of search functionality, with user identification

--- spirit/search/views.py
+++ spirit/search/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.http import request
+from haystack.inputs import Raw
 from haystack.query import SearchQuerySet
 from haystack.views import SearchView as BaseSearchView
+from haystack.query import SQ
 from djconfig import config
 
 from django.utils.decorators import method_decorator
@@ -29,11 +31,22 @@ class SearchView(BaseSearchView):
 
     def build_page(self):
         paginator = None
+        results = self.results.filter((SQ(user__contains=self.request.user.id) | SQ(private='false')), SQ(removed='false'))
+        for result in self.results:
+            print(result.user)
+            print(str(self.request.user.id))
         page = yt_paginate(
-            self.results,
+            results,
             per_page=config.topics_per_page,
             page_number=self.request.GET.get('page', 1))
         page = [
             {'fields': r.get_stored_fields(), 'pk': r.pk}
             for r in page]
         return paginator, page
+    
+    def extra_context(self):
+        extra = super(SearchView, self).extra_context()
+
+        extra['user'] = self.request.user
+
+        return extra
