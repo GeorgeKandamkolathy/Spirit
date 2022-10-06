7ee442024c19dd6bc82277cd20281ebeefe40d6d
george <george.kandamkoalthy@gmail.com>
2022-10-06 23:21:13 +1100

    Topic creation now references subcategories in creation page. Category urls no longer have the pk as a part of the slug

--- spirit/category/urls.py
+++ spirit/category/urls.py
@@ -9,6 +9,5 @@ app_name = 'category'
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
 
-    re_path(r'^(?P<pk>[0-9]+)/$', views.detail, kwargs={'slug': "", }, name='detail'),
-    re_path(r'^(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.detail, name='detail'),
+    re_path(r'(?P<slug>[\w-]+)/$', views.detail, name='detail'),
 ]
